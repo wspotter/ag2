afb227c8c2d901070a614e621c1c9cfc7bbf0e81
Lazaros Toumanidis <laztoum@protonmail.com>
2024-12-02 08:17:57 +0200

    update: llamaindex_conversable_agent.py

--- autogen/agentchat/contrib/llamaindex_conversable_agent.py
+++ autogen/agentchat/contrib/llamaindex_conversable_agent.py
@@ -16,14 +16,16 @@ try:
     from llama_index.core.agent.runner.base import AgentRunner
     from llama_index.core.base.llms.types import ChatMessage
     from llama_index.core.chat_engine.types import AgentChatResponse
-    from pydantic import BaseModel, __version__ as pydantic_version
-    
+    from pydantic import BaseModel
+    from pydantic import __version__ as pydantic_version
+
     # let's Avoid: AttributeError: type object 'Config' has no attribute 'copy'
     if pydantic_version >= "2.0":
         from pydantic import ConfigDict
 
         Config = ConfigDict(arbitrary_types_allowed=True)
     else:
+
         class Config:
             arbitrary_types_allowed = True
 
