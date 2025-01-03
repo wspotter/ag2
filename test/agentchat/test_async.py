5d3c0d89d619205b100a16dcfb2e256a980ac2c7
Robert Jambrecic <robert@airt.ai>
2025-01-03 14:14:18 +0100

    Add missing init files

--- test/agentchat/test_async.py
+++ test/agentchat/test_async.py
@@ -7,17 +7,13 @@
 #!/usr/bin/env python3 -m pytest
 
 import asyncio
-import os
-import sys
 
 import pytest
 
 import autogen
 
+from ..conftest import reason, skip_openai  # noqa: E402
 from .test_assistant_agent import KEY_LOC, OAI_CONFIG_LIST
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from conftest import reason, skip_openai  # noqa: E402
 
 
 def get_market_news(ind, ind_upper):
--- test/io/__init__.py
+++ test/io/__init__.py
@@ -1,3 +1,5 @@
+#! /bin/env bash
+
 # Copyright (c) 2023 - 2024, Owners of https://github.com/ag2ai
 #
 # SPDX-License-Identifier: Apache-2.0
