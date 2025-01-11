a1cd702fc6353bcf98b6508ae3fc7919c6a87f3d
Kumaran Rajendhiran <kumaran@airt.ai>
2025-01-11 10:04:54 +0000

    Use explicit imports in init instead of import * and use __all__

--- autogen/__init__.py
+++ autogen/__init__.py
@@ -6,12 +6,98 @@
 # SPDX-License-Identifier: MIT
 import logging
 
-from .agentchat import *
-from .code_utils import DEFAULT_MODEL, FAST_MODEL  # noqa: F401
-from .exception_utils import *
-from .oai import *
-from .version import __version__  # noqa: F401
+from .agentchat import (
+    AFTER_WORK,
+    ON_CONDITION,
+    UPDATE_SYSTEM_MESSAGE,
+    AfterWorkOption,
+    Agent,
+    AssistantAgent,
+    ChatResult,
+    ConversableAgent,
+    GroupChat,
+    GroupChatManager,
+    ReasoningAgent,
+    SwarmAgent,
+    SwarmResult,
+    ThinkNode,
+    UserProxyAgent,
+    a_initiate_swarm_chat,
+    gather_usage_summary,
+    initiate_chats,
+    initiate_swarm_chat,
+    register_function,
+    visualize_tree,
+)
+from .code_utils import DEFAULT_MODEL, FAST_MODEL
+from .exception_utils import (
+    AgentNameConflict,
+    InvalidCarryOverType,
+    NoEligibleSpeaker,
+    SenderRequired,
+    UndefinedNextAgent,
+)
+from .oai import (
+    Cache,
+    ChatCompletion,
+    Completion,
+    ModelClient,
+    OpenAIWrapper,
+    config_list_from_dotenv,
+    config_list_from_json,
+    config_list_from_models,
+    config_list_gpt4_gpt35,
+    config_list_openai_aoai,
+    filter_config,
+    get_config_list,
+)
+from .version import __version__
 
 # Set the root logger.
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+__all__ = [
+    "Agent",
+    "ConversableAgent",
+    "AssistantAgent",
+    "UserProxyAgent",
+    "GroupChat",
+    "GroupChatManager",
+    "register_function",
+    "initiate_chats",
+    "gather_usage_summary",
+    "ChatResult",
+    "initiate_swarm_chat",
+    "a_initiate_swarm_chat",
+    "SwarmAgent",
+    "SwarmResult",
+    "ON_CONDITION",
+    "AFTER_WORK",
+    "AfterWorkOption",
+    "UPDATE_SYSTEM_MESSAGE",
+    "ReasoningAgent",
+    "visualize_tree",
+    "ThinkNode",
+    "DEFAULT_MODEL",
+    "FAST_MODEL",
+    "__version__",
+    "AgentNameConflict",
+    "NoEligibleSpeaker",
+    "SenderRequired",
+    "InvalidCarryOverType",
+    "UndefinedNextAgent",
+    "OpenAIWrapper",
+    "ModelClient",
+    "Completion",
+    "ChatCompletion",
+    "get_config_list",
+    "config_list_gpt4_gpt35",
+    "config_list_openai_aoai",
+    "config_list_from_models",
+    "config_list_from_json",
+    "config_list_from_dotenv",
+    "filter_config",
+    "Cache",
+]
