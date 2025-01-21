0881a9e312aa05b1b7fbf8c77fb64fe8d1b7826c
Yiran Wu <32823396+yiranwu0@users.noreply.github.com>
2025-01-21 16:06:18 -0500

    fix (#592)
    Co-authored-by: Yiran Wu <32823396+kevin666aa@users.noreply.github.com>

--- autogen/agentchat/contrib/swarm_agent.py
+++ autogen/agentchat/contrib/swarm_agent.py
@@ -223,7 +223,7 @@ def _process_initial_messages(
     temp_user_proxy = None
     temp_user_list = []
     if len(messages) == 1 and "name" not in messages[0] and not user_agent:
-        temp_user_proxy = UserProxyAgent(name="_User")
+        temp_user_proxy = UserProxyAgent(name="_User", code_execution_config=False)
         last_agent = temp_user_proxy
         temp_user_list.append(temp_user_proxy)
     else:
