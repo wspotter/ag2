0a51416813509424f9cbd9991abf9379b98410d6
Davor Runje <davor@airt.ai>
2025-01-21 22:59:20 +0100

    fix failing tests with optional imports

--- test/cache/test_cache.py
+++ test/cache/test_cache.py
@@ -15,7 +15,7 @@ from autogen.import_utils import optional_import_block
 with optional_import_block() as result:
     from azure.cosmos import CosmosClient
 
-skip_azure = not result.is_successful
+skip_azure_cosmos = not result.is_successful
 
 
 class TestCache(unittest.TestCase):
@@ -31,7 +31,7 @@ class TestCache(unittest.TestCase):
                 "database_id": "autogen_cache",
                 "container_id": "TestContainer",
                 "cache_seed": "42",
-                "client": MagicMock(spec=CosmosClient),
+                "client": MagicMock(spec=CosmosClient) if not skip_azure_cosmos else MagicMock(),
             }
         }
 
@@ -42,7 +42,7 @@ class TestCache(unittest.TestCase):
         mock_cache_factory.assert_called()
 
     @patch("autogen.cache.cache_factory.CacheFactory.cache_factory", return_value=MagicMock())
-    @unittest.skipIf(skip_azure, "requires azure.cosmos")
+    @unittest.skipIf(skip_azure_cosmos, "requires azure.cosmos")
     def test_cosmosdb_cache_initialization(self, mock_cache_factory):
         cache = Cache(self.cosmos_config)
         self.assertIsInstance(cache.cache, MagicMock)
@@ -71,7 +71,7 @@ class TestCache(unittest.TestCase):
     def test_redis_context_manager(self):
         self.context_manager_common(self.redis_config)
 
-    @unittest.skipIf(skip_azure, "requires azure.cosmos")
+    @unittest.skipIf(skip_azure_cosmos, "requires azure.cosmos")
     def test_cosmos_context_manager(self):
         self.context_manager_common(self.cosmos_config)
 
@@ -90,7 +90,7 @@ class TestCache(unittest.TestCase):
     def test_redis_get_set(self):
         self.get_set_common(self.redis_config)
 
-    @unittest.skipIf(skip_azure, "requires azure.cosmos")
+    @unittest.skipIf(skip_azure_cosmos, "requires azure.cosmos")
     def test_cosmos_get_set(self):
         self.get_set_common(self.cosmos_config)
 
@@ -104,7 +104,7 @@ class TestCache(unittest.TestCase):
     def test_redis_close(self):
         self.close_common(self.redis_config)
 
-    @unittest.skipIf(skip_azure, "requires azure.cosmos")
+    @unittest.skipIf(skip_azure_cosmos, "requires azure.cosmos")
     def test_cosmos_close(self):
         self.close_common(self.cosmos_config)
 
