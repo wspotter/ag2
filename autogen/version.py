47a20c49dbf2498fb4d960e3fde109969e742636
Mark Sze <66362098+marklysze@users.noreply.github.com>
2025-01-23 06:58:31 +1100

    Update version to v0.7.2b1 (#623)
    * Update version to v0.7.2
    
    * Update version.py to b1
    
    * Update year

--- autogen/version.py
+++ autogen/version.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2023 - 2024, Owners of https://github.com/ag2ai
+# Copyright (c) 2023 - 2025, Owners of https://github.com/ag2ai
 #
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "0.7.1"
+__version__ = "0.7.2b1"
