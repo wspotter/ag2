e23f09fdae70921bb3c2cce80b488cfd55cdcef4
Mark Sze <mark@sze.family>
2025-02-03 21:59:45 +0000

    PeerChannel type update for Telegram
    Signed-off-by: Mark Sze <mark@sze.family>

--- autogen/tools/experimental/messageplatform/telegram/telegram.py
+++ autogen/tools/experimental/messageplatform/telegram/telegram.py
@@ -14,7 +14,9 @@ __all__ = ["TelegramRetrieveTool", "TelegramSendTool"]
 
 with optional_import_block():
     from telethon import TelegramClient
-    from telethon.tl.types import InputMessagesFilterEmpty, Message, PeerChannel, PeerChat, PeerUser
+    from telethon.tl.types import InputMessagesFilterEmpty, Message, PeerChannel, PeerChat, PeerUser  # type: ignore
+
+PeerType = Union[PeerChannel, PeerChat, PeerUser]  # type: ignore
 
 MAX_MESSAGE_LENGTH = 4096
 
@@ -28,7 +30,7 @@ class BaseTelegramTool:
         self._client = TelegramClient(session_name, api_id, api_hash)
 
     @staticmethod
-    def _get_peer_from_id(chat_id: str) -> Union[PeerChannel, PeerChat, PeerUser]:  # type: ignore[no-any-unimported]
+    def _get_peer_from_id(chat_id: str) -> PeerType:
         """Convert a chat ID string to appropriate Peer type."""
         try:
             # Convert string to integer
