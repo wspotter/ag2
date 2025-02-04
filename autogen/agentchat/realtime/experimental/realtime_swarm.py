56d871a8663029930b22f69275c39618675601d7
Tvrtko Sternak <117077296+sternakt@users.noreply.github.com>
2025-02-04 14:37:32 +0100

    Hotfix failing isinstance checks for swarmable agent (#804)
    * Remove sensitive prints
    
    * Add missing params to swarmable agent
    
    * Revert changes
    
    * Revert changes

--- autogen/agentchat/realtime/experimental/realtime_swarm.py
+++ autogen/agentchat/realtime/experimental/realtime_swarm.py
@@ -107,6 +107,8 @@ def parse_oai_message(message: Union[dict[str, Any], str], role: str, adressee: 
 class SwarmableAgent:
     """A class for an agent that can participate in a swarm chat."""
 
+    __exported_module__ = ""
+
     def __init__(
         self,
         name: str,
