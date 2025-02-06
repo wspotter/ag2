95daf1bd63b88394a053ca0b5160bcd33522dbab
Lazaros Toumanidis <lazToum@users.noreply.github.com>
2025-02-07 01:57:13 +0200

    update captain agent tools (#802)
    * update captain agent tools
    
    * lint

--- autogen/agentchat/contrib/captainagent/tools/information_retrieval/image_qa.py
+++ autogen/agentchat/contrib/captainagent/tools/information_retrieval/image_qa.py
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 import os
 
-from ......coding.func_with_reqs import with_requirements
+from autogen.coding.func_with_reqs import with_requirements
 
 
-@with_requirements(["transformers", "torch", "PIL"], ["transformers", "torch", "os"])
+@with_requirements(["transformers", "torch", "pillow"], ["transformers", "torch", "os"])
 def image_qa(image, question, ckpt="Salesforce/blip-vqa-base"):
     """Perform question answering on an image using a pre-trained VQA model.
 
