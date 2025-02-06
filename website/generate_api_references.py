f7c309a229becb53b597cfe8faf4605a9686f65f
Harish Mohan Raj <harish@airt.ai>
2025-02-06 16:28:21 +0530

    Fix the export_module decerator to update the __module__ attribute of a symbol only when building the docs (#842)

--- website/generate_api_references.py
+++ website/generate_api_references.py
@@ -4,7 +4,27 @@
 
 from __future__ import annotations
 
-from autogen._website.generate_api_references import main
+import os
+from contextlib import contextmanager
+from typing import Generator
+
+
+@contextmanager
+def _add_pdoc_placeholder_env() -> Generator[None, None, None]:
+    """Add the environment variable ADD_PDOC_PLACEHOLDER_TO_MODULE to the current environment.
+
+    The export_module decorator in doc_utils.py uses this environment variable, if set, the decorator modifies the __module__ attribute of the decorated symbol.
+    If the environment variable is not set, the decorator does not modify the __module__ attribute.
+    """
+    os.environ["ADD_PDOC_PLACEHOLDER_TO_MODULE"] = "1"
+    try:
+        yield
+    finally:
+        os.environ.pop("ADD_PDOC_PLACEHOLDER_TO_MODULE")
+
 
 if __name__ == "__main__":
-    main()
+    with _add_pdoc_placeholder_env():
+        from autogen._website.generate_api_references import main
+
+        main()
