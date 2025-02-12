9f3dfeeed83d2001e32eaab9d5b4c7eccfdb6a30
Davorin Ruševljan <davorin.rusevljan@gmail.com>
2025-02-12 15:08:53 +0100

    bump js-client in notebooks (#939)

--- notebook/agentchat_realtime_webrtc/static/main.js
+++ notebook/agentchat_realtime_webrtc/static/main.js
@@ -1,9 +1,17 @@
 const main = async () => {
     const eConnecting = document.getElementById("connecting")
     const eConnected = document.getElementById("connected")
+    const eDisconnected = document.getElementById("disconnected")
+
     eConnecting.style.display = "block"
     eConnected.style.display = "none"
+    eDisconnected.style.display = "none"
     const webRTC = new ag2client.WebRTC(socketUrl)
+    webRTC.onDisconnect = () => {
+        eDisconnected.style.display = "block"
+        eConnected.style.display = "none"
+        eConnecting.style.display = "none"
+    }
     await webRTC.connect();
     eConnecting.style.display = "none"
     eConnected.style.display = "block"
