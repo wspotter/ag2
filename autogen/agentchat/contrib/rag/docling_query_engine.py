7559cb0c85b8efc840b29c629a11356adaaf2d70
Jiacheng Shang <63379793+Eric-Shang@users.noreply.github.com>
2025-02-12 09:55:38 -0800

    Update query engine and notebook (#941)

--- autogen/agentchat/contrib/rag/docling_query_engine.py
+++ autogen/agentchat/contrib/rag/docling_query_engine.py
@@ -30,10 +30,10 @@ logger = logging.getLogger(__name__)
 @require_optional_import(["chromadb", "llama_index"], "rag")
 class DoclingMdQueryEngine:
     """
-    This query engine leverages LlamaIndex's VectorStoreIndex in combination with Chromadb to query Markdown files processed by docling.
-    You can specify the collection name when initializing the database via the init_db() method and later add more documents to the collection.
-    Data is persisted using Chromadb's PersistentClient, which saves your collections to a directory relative to your current path.
-    LlamaIndex's VectorStoreIndex is then used to efficiently index and query the documents.
+    This engine leverages Chromadb to persist document embeddings in a named collection
+    and LlamaIndex’s VectorStoreIndex to efficiently index and retrieve documents, and generate an answer in response
+    to natural language queries. The Chromadb collection serves as the storage layer, while
+    the collection name uniquely identifies the set of documents within the persistent database.
     """
 
     def __init__(  # type: ignore
@@ -46,12 +46,15 @@ class DoclingMdQueryEngine:
         """
         Initializes the DoclingMdQueryEngine with db_path, metadata, and embedding function and llm.
         Args:
-            db_path: the path to save chromadb data
-            embedding_function: The embedding function to use. Default embedding uses Sentence Transformers model all-MiniLM-L6-v2.
+            db_path: The file system path where Chromadb will store its persistent data.
+                If not specified, the default directory "./chroma" is used.
+            embedding_function: A callable that converts text into vector embeddings. Default embedding uses Sentence Transformers model all-MiniLM-L6-v2.
                 For more embeddings that ChromaDB support, please refer to [embeddings](https://docs.trychroma.com/docs/embeddings/embedding-functions)
-            metadata: The metadata used by Chromadb collection creation. Chromadb uses HNSW indexing algorithm under the hood.
+            metadata: A dictionary containing configuration parameters for the Chromadb collection.
+                This metadata is typically used to configure the HNSW indexing algorithm.
                 For more details about the default metadata, please refer to [HNSW configuration](https://cookbook.chromadb.dev/core/configuration/#hnsw-configuration)
-            llm: LLM model used by LlamaIndex. You can find more supported LLMs at [LLM](https://docs.llamaindex.ai/en/stable/module_guides/models/llms/)
+            llm: LLM model used by LlamaIndex for query processing.
+                 You can find more supported LLMs at [LLM](https://docs.llamaindex.ai/en/stable/module_guides/models/llms/)
         """
         self.llm: LLM = llm or OpenAI(model="gpt-4o", temperature=0.0)  # type: ignore[no-any-unimported]
         self.embedding_function: EmbeddingFunction[Any] = embedding_function or DefaultEmbeddingFunction()  # type: ignore[no-any-unimported,assignment]
@@ -69,15 +72,31 @@ class DoclingMdQueryEngine:
         collection_name: Optional[str] = None,
     ) -> None:
         """
-        Initialize VectorDB by creating collection using given name,
-        loading docs and creating index
+        Initialize the database for document indexing.
+
+        Creates (or retrieves) a Chromadb collection using the provided collection name,
+        loads documents from a directory or a list of file paths, and builds the vector index
+        to enable efficient querying.
+
+        Args:
+            input_dir: The directory path from which to load documents.
+                The directory should contain Docling-parsed Markdown files.
+                If not provided, no directory-based documents are loaded.
+            input_doc_paths: A list of file paths to individual documents.
+                Each file should be a Docling-parsed Markdown file.
+                If not provided, no file-based documents are loaded.
+            collection_name: The unique name for the Chromadb collection.
+                This name identifies the collection that saves the embeddings of input documents.
+                If the collection name pre-exists, this method will
+                    retrieve the collection instead of creating a new one.
+                If omitted, the default name ("docling-parsed-docs") is used.
         """
         input_dir = input_dir or ""
         input_doc_paths = input_doc_paths or []
-        collection_name = collection_name or DEFAULT_COLLECTION_NAME
+        self.collection_name = collection_name or DEFAULT_COLLECTION_NAME
 
         self.collection = self.client.create_collection(
-            name=collection_name,
+            name=self.collection_name,
             embedding_function=self.embedding_function,
             metadata=self.metadata,
             get_or_create=True,  # If collection already exists, get the collection
@@ -92,16 +111,31 @@ class DoclingMdQueryEngine:
 
     def query(self, question: str) -> str:
         """
-        Query your Docling parsed md files.
+        Retrieve information from indexed documents by processing a natural language query.
+
+        Args:
+            question: A natural language query string used to search the indexed documents.
+
+        Returns:
+            A string containing the response generated by LLM.
         """
         self.query_engine = self.index.as_query_engine(llm=self.llm)
         response = self.query_engine.query(question)
 
         return str(response)
 
-    def add_docs(self, new_doc_dir: Optional[str], new_doc_paths: Optional[list[str]]) -> None:
+    def add_docs(self, new_doc_dir: Optional[str] = "", new_doc_paths: Optional[list[str]] = []) -> None:
         """
-        Add new documents to the index from either a directory or a list of file paths.
+        Add additional documents to the existing vector index.
+
+        Loads new Docling-parsed Markdown files from a specified directory or a list of file paths
+        and inserts them into the current index for future queries.
+
+        Args:
+            new_doc_dir: The directory path from which to load additional documents.
+                If provided, all eligible files in this directory are loaded.
+            new_doc_paths: A list of file paths specifying additional documents to load.
+                Each file should be a Docling-parsed Markdown file.
         """
         new_docs = self._load_doc(input_dir=new_doc_dir, input_docs=new_doc_paths)
         for doc in new_docs:
@@ -111,9 +145,25 @@ class DoclingMdQueryEngine:
         self, input_dir: Optional[str], input_docs: Optional[list[str]]
     ) -> list["LlamaDocument"]:
         """
-        Load documents from the input directory and/or a list of input files, if provided.
-        It supports lots of [formats](https://docs.llamaindex.ai/en/stable/module_guides/loading/simpledirectoryreader/#supported-file-types),
-          but you should use Docling parsed Markdown files for this query engine.
+        Load documents from a directory and/or a list of file paths.
+
+        This helper method reads Docling-parsed Markdown files using LlamaIndex's
+        SimpleDirectoryReader. It supports multiple file [formats]((https://docs.llamaindex.ai/en/stable/module_guides/loading/simpledirectoryreader/#supported-file-types)),
+          but the intended use is for documents processed by Docling.
+
+        Args:
+            input_dir: The directory containing documents to be loaded.
+                If provided, all files in the directory will be considered.
+            input_docs: A list of individual file paths to load.
+                Each path must point to an existing file.
+
+        Returns:
+            A list of documents loaded as LlamaDocument objects.
+
+        Raises:
+            ValueError: If the specified directory does not exist.
+            ValueError: If any provided file path does not exist.
+            ValueError: If neither input_dir nor input_docs is provided.
         """
         loaded_documents = []
         if input_dir:
@@ -138,7 +188,17 @@ class DoclingMdQueryEngine:
         self, collection: "Collection", docs: list["LlamaDocument"]
     ) -> "VectorStoreIndex":
         """
-        Create a LlamaIndex VectorStoreIndex using the provided documents and Chromadb collection.
+        Build a vector index for document retrieval using a Chromadb collection.
+
+        Wraps the provided Chromadb collection into a vector store and uses LlamaIndex's
+        StorageContext to create a VectorStoreIndex from the supplied documents.
+
+        Args:
+            collection: A Chromadb Collection object that stores the embeddings of the documents.
+            docs: A list of LlamaDocument objects representing the documents to be indexed.
+
+        Returns:
+            A VectorStoreIndex object built from the provided documents and backed by the given collection.
         """
 
         self.vector_store = ChromaVectorStore(chroma_collection=collection)
@@ -147,3 +207,14 @@ class DoclingMdQueryEngine:
         index = VectorStoreIndex.from_documents(docs, storage_context=self.storage_context)
 
         return index
+
+    def get_collection_name(self) -> Optional[str]:
+        """
+        Retrieve the name of the Chromadb collection used for indexing.
+
+        Returns:
+            The name of the Chromadb collection as a string, or None if no index exists.
+        """
+        if self.index:
+            return self.collection_name
+        return None
