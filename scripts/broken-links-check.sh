afb54ad2e8551b306928dd2676333794d5af606a
Kumaran Rajendhiran <kumaran@airt.ai>
2025-02-18 18:51:03 +0530

    Fix check broken links (#1016)
    * Breakdown for readability
    
    * Debug: run in current branch
    
    * Debug: disable if condition
    
    * Ignore all the edit URLs
    
    * Use verbose
    
    * Remove verbose
    
    * Remove _site as it is removed and returning 308 redirect
    
    * Trigger workflow for docs script changes and excluded link changes
    
    * Remove github links from exclusion list
    
    * Add all _sites to exclusion list
    
    * Ignore anchor tags in github links
    
    * Debug: Remove almost all the links
    
    * Remove empty lines
    
    * Update excluded domains
    
    * Remove sleep
    
    * Add few more links for exclusion
    
    * Remove debug stuff

--- scripts/broken-links-check.sh
+++ scripts/broken-links-check.sh
@@ -7,4 +7,13 @@
 set -o errexit
 
 # Run muffet to check for broken links
-muffet --buffer-size=16384 --max-connections=1 --color=always --header="User-Agent:Mozilla/5.0(Firefox/97.0)"  --max-connections-per-host=1 --rate-limit=1 --max-response-body-size=20000000 --exclude="($(paste -sd '|' .muffet-excluded-links.txt))" https://docs.ag2.ai/docs/Home
+muffet \
+  --buffer-size=16384 \
+  --max-connections=1 \
+  --color=always \
+  --header="User-Agent:Mozilla/5.0(Firefox/97.0)" \
+  --max-connections-per-host=1 \
+  --rate-limit=1 \
+  --max-response-body-size=20000000 \
+  --exclude="($(paste -sd '|' .muffet-excluded-links.txt))" \
+  https://docs.ag2.ai/docs/Home
