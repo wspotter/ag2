9cd2ef475bf60c2d87faceee610cc9a3245887fc
Harish Mohan Raj <harish@airt.ai>
2025-05-07 08:11:36 +0530

    Docs polishing (#1797)

--- scripts/broken-links-check.sh
+++ scripts/broken-links-check.sh
@@ -34,6 +34,6 @@ muffet \
   --rate-limit=1 \
   --max-response-body-size=20000000 \
   --ignore-fragments \
-  --accepted-status-codes='200..300,503,429' \
+  --accepted-status-codes='200..300,503,403,429' \
   --exclude="($(paste -sd '|' .muffet-excluded-links.txt))" \
   "$URL"
